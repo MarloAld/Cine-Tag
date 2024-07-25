fe8e6054f88284d67e16fdff56739fa33376a3b5
Marlinton Aldrighi <marlinton13@gmail.com>
2024-07-24 21:31:34 -0300

    First class

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,15 @@
+import Rodape from "components/Rodape";
+
+const { default: Cabecalho } = require("components/Cabecalho");
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+
+            <Rodape />
+        </>
+    )
+}
+
+export default Inicio
